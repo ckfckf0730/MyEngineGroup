d9120adacf54115a4652c0a9cbe993ad143158b2
ckfckf0730 <39393418+ckfckf0730@users.noreply.github.com>
2024-04-02 14:05:05 +0900

    Add Select Project Title Form.

---
+++ MyEngine/MyEngine/Forms/TitleForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CkfEngine.Forms
+{
+    partial class TitleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonNew = new System.Windows.Forms.Button();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonNew
+            // 
+            this.buttonNew.Location = new System.Drawing.Point(215, 84);
+            this.buttonNew.Name = "buttonNew";
+            this.buttonNew.Size = new System.Drawing.Size(320, 80);
+            this.buttonNew.TabIndex = 0;
+            this.buttonNew.Text = "Create New Project";
+            this.buttonNew.UseVisualStyleBackColor = true;
+            this.buttonNew.Click += new System.EventHandler(this.buttonNew_Click);
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(215, 195);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(320, 80);
+            this.buttonSelect.TabIndex = 1;
+            this.buttonSelect.Text = "Select Project";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // TitleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.buttonNew);
+            this.Name = "TitleForm";
+            this.Text = "TitleForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonNew;
+        private System.Windows.Forms.Button buttonSelect;
+    }
+}
---
+++ MyEngine/MyEngine/Forms/TitleForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CkfEngine.Forms
+{
+    public partial class TitleForm : Form
+    {
+        public TitleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Select a File";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+   
+
+
+            }
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
